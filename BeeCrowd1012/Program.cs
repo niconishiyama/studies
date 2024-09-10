8f8a449bf028bd08d3cd2873979de4fb2c835538
niconishiyama <nico_dev@outlook.com>
2024-09-10 19:48:36 -0300

    Exercicio Aula 26 URI 1012

---
+++ BeeCrowd1012/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeeCrowd1012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            double triangulo, circulo, trapezio, quadrado, retangulo, pi;
+
+            string[] values = Console.ReadLine().Split(' ');
+                double A = double.Parse(values[0]);
+                double B = double.Parse(values[1]);
+                double C = double.Parse(values[2]);
+
+            pi = 3.14159;
+            triangulo = A * C / 2.0;
+            circulo = pi * Math.Pow(C, 2.0);
+            trapezio = (A + B) * C / 2.0;
+            quadrado = Math.Pow(B, 2.0);
+            retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: {0}", triangulo.ToString("F3"));
+            Console.WriteLine("CIRCULO: {0}", circulo.ToString("F3"));
+            Console.WriteLine("TRAPEZIO: {0}", trapezio.ToString("F3"));
+            Console.WriteLine("QUADRADO: {0}", quadrado.ToString("F3"));
+            Console.WriteLine("RETANGULO: {0}", retangulo.ToString("F3"));
+        }
+    }
+}
