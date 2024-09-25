4f3e9fcf2f44a1ed3d3a21023b629d1182e735d1
niconishiyama <nico_dev@outlook.com>
2024-09-24 21:39:53 -0300

    BeeCrowd1019

---
+++ BeeCrowd1019/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeCrowd1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours, minutes, seconds, time;
+
+            time = int.Parse(Console.ReadLine());
+
+            seconds = time % 60;
+            minutes = time / 60 % 60;
+            hours = time / 3600;
+
+            Console.WriteLine("{0}:{1}:{2}", hours, minutes, seconds);
+        }
+    }
+}
